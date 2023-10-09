ad289e392198373e6acebde7b2721ccf4a00219f
BAHAA-THE-KING <bahaaalnokta2003@gmail.com>
2023-10-09 19:42:19 +0300

    Change Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import normal from "./styles/normal";
 
 const items = [
    {
@@ -22,33 +21,60 @@ const items = [
 
 const Header = () => {
    return (
-      <header className="w-full h-16 bg-slate-200 flex justify-start items-center">
-         {
-            items.map(
-               e => (
-                  <Link
-                     key={e.title}
-                     href={e.url}
-                     className={`
-                                 w-28
-                                 h-min
-                                 p-2
-                                 m-3
-                                 text-center
+      <div className="
+                     navbar
+                     bg-purple-200
 
-                                 bg-white
-                                 
-                                 ${normal}
-                                 
-                                 rounded-xl
-                                 `}
-                  >
-                     {e.title}
-                  </Link>
-               )
-            )
-         }
-      </header>
+                     text-black
+
+                     border
+                     border-purple-600
+                     rounded-xl
+                     rounded-t-none
+                     
+                     transition-all
+                     hover:bg-purple-600
+                     hover:text-slate-200
+                     ">
+         <div className="navbar-start">
+            <div className="dropdown">
+               <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+               </label>
+               <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                  {
+                     items.map(
+                        item =>
+                           <li>
+                              <Link href={item.url}>{item.title}</Link>
+                           </li>
+                     )
+                  }
+               </ul>
+            </div>
+            <a className="btn btn-ghost normal-case text-xl">E-Commerce</a>
+         </div>
+         <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+               {
+                  items.map(
+                     item =>
+                        <li>
+                           <Link href={item.url}>{item.title}</Link>
+                        </li>
+                  )
+               }
+            </ul>
+         </div>
+         <div className="navbar-end">
+            <button className="btn btn-ghost btn-circle">
+               <div className="indicator">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+                  <span className="badge badge-xs badge-primary indicator-item"></span>
+               </div>
+            </button>
+         </div>
+      </div>
    );
 }
 
